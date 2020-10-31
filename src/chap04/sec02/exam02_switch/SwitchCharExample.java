9070d85fe6e44788c162dc6dfd3a2489b5388064
bom77 <bom7768@gmail.com>
2020-10-31 20:05:57 +0900

    2020 10 31

--- src/chap04/sec02/exam02_switch/SwitchCharExample.java
+++ src/chap04/sec02/exam02_switch/SwitchCharExample.java
@@ -1,5 +1,18 @@
 package chap04.sec02.exam02_switch;
 
 public class SwitchCharExample {
+	public static void main(String[] args) {
+		char grade = 'B';
 
+		switch (grade) {
+		case 'A':
+			break;
+		case 'a':
+			System.out.println("우수 회원입니다");
+			break;
+		case 'B':
+		case 'b':
+		default:
+		}
+	}
 }
